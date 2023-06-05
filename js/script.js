f09d203acf4e4a5e73aca0a416274380c096a671
pluggarose <poljekson@mail.ru>
2023-06-05 20:13:41 +0300

    Код оформлен в обьектном стиле

--- js/script.js
+++ js/script.js
@@ -1,52 +1,55 @@
-const data = [{
+const skill = {
+    data: [{
         item: "html",
-        level: 100,
+        level: 15,
         iconPath: "img/html.svg",
     },
     {
         item: "css",
-        level: 100,
+        level: 15,
         iconPath: "img/css.svg",
     },
     {
         item: "figma",
-        level: 100,
+        level: 15,
         iconPath: "img/figma.svg",
     },
         {item: "C++",
-        level: 100,
+        level: 15,
         iconPath: "img/c++.svg",
     },
     {
         item: "java",
-        level: 100,
+        level: 15,
         iconPath: "img/photoshop.svg",
     },
         {item: "photoshop",
-        level: 100,
+        level: 15,
         iconPath: "img/c++.svg",
-    }
-];
+    }],
+    generateList: function(parentElement) {
+        this.data.forEach(function(element) {
+            const dtt = document.createElement('dt');
+            dtt.classList.add('skill-item');
+            dtt.textContent = element.item;
+            dtt.style.backgroundImage = `url(${element.iconPath})`;
 
-const skillList = document.querySelector("dl.skill-list");
-
-data.forEach(element => {
-    const dtt = document.createElement('dt');
-    dtt.classList.add('skill-item');
-    dtt.textContent = element.item;
-    dtt.style.backgroundImage = `url(${element.iconPath})`;
-
-    const ddd = document.createElement('dd');
-    ddd.classList.add('skill-level');
+            const ddd = document.createElement('dd');
+            ddd.classList.add('skill-level');
             
-    const divv = document.createElement('div');
-    divv.style.width = `${element.level}%`;
-    divv.textContent = `${element.level}%`;
+            const divv = document.createElement('div');
+            divv.style.width = `${element.level}%`;
+            divv.textContent = `${element.level}%`;
 
-    ddd.appendChild(divv);
-    skillList.appendChild(dtt);
-    skillList.appendChild(ddd);
-});
+            ddd.appendChild(divv);
+            parentElement.appendChild(dtt);
+            parentElement.appendChild(ddd);
+        });
+    }
+};
+
+skill.generateList(document.querySelector("dl.skill-list"));
+
 
 
 
