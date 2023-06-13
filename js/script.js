df772005d01b3ec8883d2c95e51570cc6f4b9579
pluggarose <poljekson@mail.ru>
2023-06-13 16:06:51 +0300

    ТИ-10

--- js/script.js
+++ js/script.js
@@ -29,7 +29,9 @@ const skill = {
         level: 21,
         iconPath: "img/c++.svg",
     }],
+    isSorted: false,
     generateList: function(parentElement) {
+        parentElement.innerHTML = '';
         this.data.forEach(element => {
             const dtt = document.createElement('dt');
             dtt.classList.add('skill-item');
@@ -47,11 +49,58 @@ const skill = {
             
             parentElement.append(dtt,ddd);
         });
+    },
+    sortList: function(sortingType) {
+        if (skill.isSorted !== sortingType){
+            switch (sortingType){
+                case 'name':
+                    this.data.sort((a,b) => a.item.localeCompare(b.item)); break;
+                case 'level':
+                    this.data.sort((a,b) => b.level - a.level); break;
+                default:
+                    return;
+            }
+            this.isSorted = sortingType;
+        } else {
+            this.data.reverse();
+        }
+        this.generateList(document.querySelector("dl.skill-list"));
     }
 };
 
 skill.generateList(document.querySelector("dl.skill-list"));
 
 
+sortBtnBlock = document.querySelector("div.skills-sort");
+sortBtnBlock.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target.nodeName === "BUTTON")
+    {
+        skill.sortList(target.dataset.type);
+    } 
+});
+
+const nav = document.querySelector('.main-nav');
+const btn = document.querySelector('.nav-btn');
+
+const menu = {
+    open: function() {
+        nav.classList.remove('main-nav_closed');
+        btn.classList.add('nav-btn_close');
+        btn.classList.remove('nav-btn_open');
+        btn.innerHTML = '<span class="visually-hidden">Закрыть меню</span>';
+    },
+    close: function() {
+        nav.classList.add('main-nav_closed');
+        btn.classList.remove('nav-btn_close');
+        btn.classList.add('nav-btn_open');
+        btn.innerHTML = '<span class="visually-hidden">Открыть меню</span>';
+    },
+};
+
+btn.addEventListener('click', (e) => {
+    e.target.classList.contains('nav-btn_open') ? menu.open() : menu.close();
+});
+menu.close();
 
 
